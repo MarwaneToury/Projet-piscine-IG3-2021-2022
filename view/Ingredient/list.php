e8d79cc01987f5750a953016b671ee0759f57909
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2021-10-18 17:28:04 +0200

    tri par catégorie + ordre alphabétique (de cat et ing)

--- view/Ingredient/list.php
+++ view/Ingredient/list.php
@@ -5,7 +5,18 @@ echo "
 	<input type='hidden' name='action' value='create'/>
 	<button type='submit'>Ajouter un ingrédient</button>
 </form>";
+$iCode_CAT_HTML = htmlspecialchars($tab_i[0]->getCodeCAT());
+$iLibelle_CAT_HTML = htmlspecialchars($tab_i[0]->getLibelleCAT());
+echo "<h4>$iLibelle_CAT_HTML</h4>";
 foreach ($tab_i as $i) {
+
+    if ($i->getCodeCAT() != $iCode_CAT_HTML)
+    {
+        $iCode_CAT_HTML = htmlspecialchars($i->getCodeCAT());
+        $iLibelle_CAT_HTML = htmlspecialchars($i->getLibelleCAT());
+        echo "<h4>$iLibelle_CAT_HTML</h4>";
+    }
+
     $iCode_ING_URL = rawurlencode($i->getCode_ING());
     $iCode_ING_HTML = htmlspecialchars($i->getCode_ING());
     $iLibelle_ING_HTML = htmlspecialchars($i->getLibelleING());
@@ -14,12 +25,10 @@ foreach ($tab_i as $i) {
     if ($i->getEstAllergeneING() == 0) $strAllergene="Non"; else $strAllergene="Oui";
     $iEstAllergene_ING_HTML = htmlspecialchars($strAllergene);
     $iValeur_TVA_HTML = htmlspecialchars($i->getValeurTVA());
-    $iLibelle_CAT_HTML = htmlspecialchars($i->getLibelleCAT());
     $iLibelle_UNI_HTML = htmlspecialchars($i->getLibelleUNI());
 
     echo "
     <p>
-        $iLibelle_CAT_HTML
         $iLibelle_ING_HTML
         $iPrix_ING_HTML
         $iEstAllergene_ING_HTML
