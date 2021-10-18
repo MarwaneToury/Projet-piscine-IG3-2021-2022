721d3e1756677a3dace08d0b3f2c0347379b880f
marwane <tmarwane2000@gmail.com>
2021-10-18 11:57:20 +0000

    ajouts depuis la branche d'alma + qlq modifs

---
+++ controller/controllerREC.php
@@ -0,0 +1,15 @@
+<?php
+require_once File::build_path(array("model", "modelIngredient.php"));
+
+class ControllerREC {
+    protected static $object = "Recette";
+
+    public static function readall() {
+        $view='list';
+        $pagetitle='Liste des fiches techniques existantes';
+        $tab_r = modelRecette::selectAll();
+        require File::build_path(array("view", "view.php"));
+    }
+    // ça c'est pour la liste en bas à gauche qui montre toutes les FT
+    // TODO : après faudras qu'en en prenne qu'un seul ( celui qui est sélectionné et qu'on recup ses infos )
+}
---
+++ view/Ingredient/listNameREC.php
@@ -0,0 +1,22 @@
+<h2> Liste des recettes existantes </h2>
+<?php
+foreach ($tab_r as $r) { //TODO: Check si je fais pas de la merde
+    $libelle_REC_URL = rawurlencode($r->getLibelle_REC());
+    $Code_AUT_URL = rawurlencode($r->getCode_AUT());
+    $libelle_REC_HTML = htmlspecialchars($r->getLibelle_REC());
+    $Code_AUT_HTML = htmlspecialchars($r->getCode_AUT());
+    echo "
+    <p>
+        Fiche technique de libellé
+        <a href='index.php?action=read&libelle=$libelle_REC_URL'>
+            $libelle_REC_HTML
+        </a>
+    </p>";
+    echo "
+        <p>
+            Fiche technique d'auteur
+            <a href='index.php?action=read&AUT=$Code_AUT_URL'>
+                $Code_AUT_HTML
+            </a>
+        </p>";
+?>
