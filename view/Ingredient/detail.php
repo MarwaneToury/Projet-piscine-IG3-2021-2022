2a6233039e33ed1fa5d92120c88e6039335c3892
marwane <tmarwane2000@gmail.com>
2021-10-18 11:21:08 +0000

    la view detail.php conforme aux attributs de l'ingrédient

--- view/Ingredient/detail.php
+++ view/Ingredient/detail.php
@@ -1,19 +1,21 @@
 <h2> Détail de l'ingrédient </h2>
 <?php
-$vImmatriculation = htmlspecialchars($v->getImmatriculation());
-$vMarque = htmlspecialchars($v->getMarque());
-$vCouleur = htmlspecialchars($v->getCouleur());
-echo "<p> Voiture d'immatriculation $vImmatriculation, de marque $vMarque et de couleur $vCouleur</p>";
+$vCode_ING = htmlspecialchars($v->getCode_ING());
+$vLibelleING = htmlspecialchars($v->getLibelleING());
+$vPrixING = htmlspecialchars($v->getPrixING());
+$vQuantiteStockING = htmlspecialchars($v->getQuantiteStockING());
+$vEstAllergeneING = htmlspecialchars($v->getEstAllergeneING());
+echo "<p> Ingrédient numéro $vCode_ING, de libellé $vLibelleING, de prix $vPrixING, de quantité stockée $vQuantiteStockING, et est allergène : $vEstAllergeneING</p>";
 echo "
 <form method='get' action='index.php'>
 	<input type='hidden' name='action' value='update'/>
-	<input type='hidden' name='immat' value='$vImmatriculation'/>
-	<button type='submit'>Modifier la voiture</button>
+	<input type='hidden' name='codeING' value='$vCode_ING'/>
+	<button type='submit'>Modifier ce produit</button>
 </form>";
 echo "
 <form method='get' action='index.php'>
 	<input type='hidden' name='action' value='delete'/>
-	<input type='hidden' name='immat' value='$vImmatriculation'/>
-	<button type='submit'>Supprimer la voiture</button>
+	<input type='hidden' name='codeING' value='$vCode_ING'/>
+	<button type='submit'>Supprimer ce produit</button>
 </form>";
 ?>
