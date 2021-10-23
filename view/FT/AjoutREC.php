f728342183b6b3209da0a611f304afd69123fb17
Alma <alma.sorrentino@yahoo.fr>
2021-10-23 18:55:49 +0200

    Modif

--- view/FT/AjoutREC.php
+++ view/FT/AjoutREC.php
@@ -1,4 +1,4 @@
-<h2> Ajout recette </h2>
+<h3> Ajout recette </h3>
 <?php
 $Progression_REC = htmlspecialchars($v->getProgression_REC()); //TODO: je sais pas si les trucs là sont utiles ou juste le form et aussi comment on check si les champs sont correctement remplis ?
 $Libelle_REC = htmlspecialchars($v->getLibelle_REC());
@@ -7,14 +7,37 @@ $CoutPersonnel_REC = htmlspecialchars($v->getCoutPersonnel_REC());
 $Assaisonement_REC = htmlspecialchars($v->getAssaisonement_REC());
 $CoeffCout_REC = htmlspecialchars($v->getCoeffCout_REC());
 echo "
-<form method='get' action='index.php'>
-	<input type='hidden' name='action' value='create'/>
-	<input type='hidden' name='Progression' value='$Progression_REC'/>
-	<input type='hidden' name='Nom' value='$Libelle_REC'/>
-	<input type='hidden' name='ChargesSup' value='$ChargesSup_REC'/>
-	<input type='hidden' name='CoutPersonnel' value='$CoutPersonnel_REC'/>
-	<input type='hidden' name='Assaisonnement' value='$Assaisonement_REC'/>
-	<input type='hidden' name='CoeffCout' value='$CoeffCout_REC'/>
-	<button type='submit'>Créer la fiche technique</button>
+<h2> Formulaire create fiche technique </h2>
+<form method='post' action='index.php?controller=$controller&action=$action'>
+    <fieldset>
+        <legend>Mon formulaire :</legend>
+        <p>
+            <label for='Progression_REC_id'>Progression</label> :
+        	<input type='text' placeholder='Ex : Banane' value='$Progression_REC' name='Progression_REC' required/>
+        </p>
+        <p>
+            <label for='Libelle_REC_id'>Libellé fiche technique</label> :
+            <input type='text' placeholder='Ex : Banane' value='$Libelle_REC' name='Libelle_REC' required/>
+        </p>
+        <p>
+            <label for='ChargesSup_REC_id'>Charges supplémentaires</label> :
+            <input type='text' placeholder='Ex : Banane' value='$ChargesSup_REC' name='ChargesSup_REC' required/>
+        </p>
+        <p>
+           <label for='CoutPersonnel_REC_id'>Coût du personnel</label> :
+            <input type='text' placeholder='Ex : Banane' value='$CoutPersonnel_REC' name='CoutPersonnel_REC' required/>
+        </p>
+        <p>
+            <label for='CoeffCout_REC_id'>Coefficient sur le coût</label> :
+            <input type='text' placeholder='Ex : Banane' value='$CoeffCout_REC' name='CoeffCout_REC' required/>
+        </p>
+        <p>
+             <label for='Assaisonement_REC_id'>Assaisonnement</label> :
+             <input type='text' placeholder='Ex : Banane' value='$Assaisonement_REC' name='Assaisonement_REC' required/>
+        </p>
+	    <p>
+    	    <input type='submit' value='Envoyer' />
+        </p>
+    </fieldset>
 </form>";
 ?>
--- view/FT/listNameREC.php
+++ view/FT/listNameREC.php
@@ -1,10 +1,22 @@
 <h2> Liste des recettes existantes </h2>
 <?php
-foreach ($tab_r as $r) { //TODO: Check si je fais pas de la merde
+echo "
+<form method='get' action='index.php'>
+    <input type='hidden' name='controller' value='FT'
+	<input type='hidden' name='action' value='create'/>
+	<button type='submit'>Ajouter une fiche technique</button>
+</form>"; // Pas sûre de ce que j'ai mit dans value
+
+$Code_AUT_URL = rawurlencode($r->getCode_AUT());
+$Nom_AUT_URL = htmlspecialchars($tab_i[0]->getNom_AUT()); //Check l'indice dans la table
+echo "<h4>$Nom_AUT_URL</h4>";
+
+foreach ($tab_r as $r) {
     $libelle_REC_URL = rawurlencode($r->getLibelle_REC());
     $Code_AUT_URL = rawurlencode($r->getCode_AUT());
     $libelle_REC_HTML = htmlspecialchars($r->getLibelle_REC());
     $Code_AUT_HTML = htmlspecialchars($r->getCode_AUT());
+
     echo "
     <p>
         Fiche technique de libellé
