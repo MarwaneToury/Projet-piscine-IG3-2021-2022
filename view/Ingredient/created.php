0932a58328f1cae90d078d1c41898c0dd1b3aada
Alma <alma.sorrentino@yahoo.fr>
2021-10-24 16:30:26 +0200

    juste une faute d'orthographe

--- view/Ingredient/created.php
+++ view/Ingredient/created.php
@@ -1,5 +1,5 @@
 <?php
 $iLibelle_ING_HTML = htmlspecialchars($iLibelle_ING);
-echo "<p> Ingrédient '$iLibelle_ING_HTML' créée !</p>";
+echo "<p> Ingrédient '$iLibelle_ING_HTML' créé !</p>";
 require File::build_path(array("view","Ingredient","list.php"));
 ?>
