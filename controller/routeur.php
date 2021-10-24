607840c737fae42d858e06fb37508ed750e81999
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2021-10-24 20:21:05 +0200

    avancée double page

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,6 +1,10 @@
 <?php
 require_once File::build_path(array("controller", "controllerIngredient.php"));
 require_once File::build_path(array("controller", "controllerRecette.php"));
+require_once File::build_path(array("controller", "initializer.php"));
+
+if (!isset($_GET['action1'],$_GET['action2'],$_GET['controller1'],$_GET['controller2']))
+    Initializer::init();
 
 if (isset($_GET['action1']))
     $action1 = $_GET['action1'];
@@ -27,4 +31,5 @@ if (class_exists($_GET['controller2']) && in_array($action2, get_class_methods($
     $_GET['controller2']::$action2();
 else
     echo "Error, invalid action $action2";
+
 ?>
