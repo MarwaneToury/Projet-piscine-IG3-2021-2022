b1364679e388b1c24d6b859090097e34de023965
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2021-10-28 14:54:47 +0200

    merge conflicts (2)

--- model/modelReutiliser.php
+++ model/modelReutiliser.php
@@ -26,34 +26,34 @@ class ModelReutiliser extends model
         }
     }
 
-    public static function save($data)
-    {
-        try {
-            $table_name = static::$object;
-            $into = "";
-            $values = "";
-
-            //TODO: need to check this foreach too
-            foreach ($data as $key => $value) {
-                if ($key = )
-                $into = "$into$key,";
-                $values = "$values:$key,";
-            }
-            $into = rtrim($into, ",");
-            $values = rtrim($values, ",");
-
-            $sql = "INSERT INTO Reutiliser ($into) VALUES ($values)";
-            $req_prep = Model::$pdo->prepare($sql);
-            $req_prep->execute($data);
-        } catch (PDOException $e) {
-            if (Conf::getDebug()) {
-                echo $e->getMessage();
-            } else {
-                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
-            }
-            die();
-        }
-    }
+//    public static function save($data)
+//    {
+//        try {
+//            $table_name = static::$object;
+//            $into = "";
+//            $values = "";
+//
+//            //TODO: need to check this foreach too
+//            foreach ($data as $key => $value) {
+//                if ($key = )
+//                $into = "$into$key,";
+//                $values = "$values:$key,";
+//            }
+//            $into = rtrim($into, ",");
+//            $values = rtrim($values, ",");
+//
+//            $sql = "INSERT INTO Reutiliser ($into) VALUES ($values)";
+//            $req_prep = Model::$pdo->prepare($sql);
+//            $req_prep->execute($data);
+//        } catch (PDOException $e) {
+//            if (Conf::getDebug()) {
+//                echo $e->getMessage();
+//            } else {
+//                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+//            }
+//            die();
+//        }
+//    }
 }
 
 ?>
